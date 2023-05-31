449252326c247d1292204843f7a7c44b9fe45cc6
nathanaeltchamba <natanshost@gmail.com>
2023-05-30 22:52:08 -0500

    completed Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -9,40 +9,131 @@ const Nav = () => {
 
     const isUserLoggedIn = true;
 
-  return (
-    <nav className="flex-between w-full mb-16 pt-3">
-        <Link href='/' className="flex gap-2 flex-center">
-            <Image 
-            src='/assets/images/rank-logo.svg'
-            alt='RankR logo'
-            width={30}
-            height={30}
-            className="object-contain"
-            />
-            <p className="header_text blue_gradient ms-1 mt-1">RankR</p>
-        </Link>
+    const [providers, setProviders] = useState(null);
+    const [toggleDropwdown, setToggleDropdown] = useState(false);
 
-        {/* Desktop Navigation */}
-        <div className="sm:flex hidden">
-            {isUserLoggedIn ? (
-                <div className="flex gap-3 md:gap-5">
-                    <Link href="/create-group" className="blue_btn">
-                        Create Group
-                    </Link>
+    useEffect(() => {
+        const setProviers = async () => {
+            const response = await getProivders();
 
-                    <button type="button" onClick={signOut} className="outline_btn">
-                        Sign Out
-                    </button>
-                    
-                </div>
-            ): (
-                <>
+            setProviders(response);
+        }
 
-                </>
-            )}
-        </div>
-    </nav>
-  )
+        setProviders();
+    }, [])
+
+
+    return (
+        <nav className="flex-between w-full mb-16 pt-3">
+            <Link href='/' className="flex gap-2 flex-center">
+                <Image
+                    src='/assets/images/rank-logo.svg'
+                    alt='RankR logo'
+                    width={30}
+                    height={30}
+                    className="object-contain"
+                />
+                <p className="header_text blue_gradient ms-1 mt-1">RankR</p>
+            </Link>
+
+            {/* Desktop Navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-group" className="blue_btn">
+                            Create Group
+                        </Link>
+
+                        <button type="button" onClick={signOut} className="outline_btn">
+                            Sign Out
+                        </button>
+
+
+                        <Link href="/profile">
+                            <Image
+                                src="/assets/images/logo.svg"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="profile image"
+                            />
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                            >
+                                Sign In
+                            </button>
+                        ))}
+                    </>
+                )}
+            </div>
+
+            {/* Moible Navigation */}
+
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                            onClick={() => setToggleDropdown((prev) => !prev)}
+                        />
+
+                        {toggleDropwdown && (
+                            <div className="dropdown">
+                                <Link
+                                href="/profile"
+                                className="dropdown_link"
+                                onClick={() => setToggleDropdown(false)}
+                                >  
+                                Profile
+                                </Link>
+                                <Link
+                                href="/create-group"
+                                className="dropdown_link"
+                                onClick={() => setToggleDropdown(false)}
+                                >  
+                                Create Group
+                                </Link>
+
+                                <button 
+                                type="button"
+                                onClick={() => {setToggleDropdown(false);
+                                signOut();
+                                }}
+                                className="mt-5 w-full blue_btn">
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
+
+                    </div>
+                ) : (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button
+                            type="button"
+                            key={provider.name}
+                            onClick={() => signIn(provider.id)}
+                            >
+                                Sign In
+                            </button>
+                        ))}
+                    </>
+                )}
+            </div>
+
+        </nav>
+    )
 }
 
 export default Nav
